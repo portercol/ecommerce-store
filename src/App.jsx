2af6a75f677a031a64b2b6c33559693fcd04f0c4
Collin Porter <colls.potta@gmail.com>
2022-04-16 13:59:22 -0600

    created react-app, removed started files from react, added components and pages (navbar comp, home page)

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import Home from '../src/pages/Home'
+
+const App = () => {
+  return <Home />;
+};
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,61 @@
+import { Search } from "@material-ui/icons";
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  height: 60px;
+  background-color: black;
+  color: white;
+`;
+
+const Wrapper = styled.div`
+  padding: 10px 20px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+`;
+
+const Language = styled.span`
+  font-size: 14px;
+  cursur: pointer;
+`;
+
+const SearchContainer = styled.div`
+  border: 1px solid black;
+  display: flex;
+  align-items: center;
+`;
+
+const Center = styled.div`
+  flex: 1;
+`;
+
+const Right = styled.div`
+  flex: 1;
+`;
+
+const Navbar = () => {
+  return (
+    <Container>
+      <Wrapper>
+        <Left>
+          <Language>EN</Language>
+          <SearchContainer>
+            input 
+            <Search />
+          </SearchContainer>
+          left
+        </Left>
+        <Center>center</Center>
+        <Right>right</Right>
+      </Wrapper>
+    </Container>
+  );
+};
+
+export default Navbar;
