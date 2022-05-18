54d59a81587ae45ccea022099258e476fbf12caf
Collin Porter <colls.potta@gmail.com>
2022-05-18 11:16:05 -0600

    created express server and test api route

---
+++ Server/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const userRoutes = require("./routes/user");
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("DB Connection Successful!"))
+  .catch((err) => {
+    console.log(err);
+  });
+
+// app.get("/api/test", () => {
+//   console.log("test is successful");
+// });
+
+app.use("/api/user", userRoutes);
+
+app.listen(process.env.PORT || 3001, () => {
+  console.log("Backend server is running!");
+});
